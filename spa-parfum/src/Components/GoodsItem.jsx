cd75dfe415fef17294f1365a98a0f60c95fd38fb
Sergey Ermolenko <ermolenko@medignition.com>
2022-05-19 10:06:46 +0300

    fixed

--- spa-parfum/src/Components/GoodsItem.jsx
+++ spa-parfum/src/Components/GoodsItem.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import PT from 'prop-types';
 
-const GoodsItem = ({ goods }) => {
-  const { id, title, price, image, category } = goods;
+const GoodsItem = ({ id, title, price, image, category }) => {
   const history = useNavigate();
-
   const handleClick = () => {
     history(`/goods/${id}`, {
       state: {
@@ -16,26 +15,27 @@ const GoodsItem = ({ goods }) => {
 
   return (
     <li className="goods__item">
-      <div className="goods__item__img">
-        <img src={image} alt={title} />
-      </div>
+      <div className="goods__item__img">{image && <img src={image} alt={title} />}</div>
       <div className="goods__caption">
-        <div
-          className="goods__caption__name"
-          onClick={() => {
-            handleClick();
-          }}
-        >
+        <button type="button" className="goods__caption__name" onClick={handleClick}>
           {title}
-        </div>
+        </button>
         <div className="goods__caption__subname">{category}</div>
         <div className="goods__caption__price">${price} $</div>
-        <button id={id} className="goods__caption__btn">
+        <button type="button" data-id={id} className="goods__caption__btn">
           +
         </button>
       </div>
     </li>
   );
+};
+
+GoodsItem.propTypes = {
+  id: PT.number.isRequired,
+  title: PT.string.isRequired,
+  price: PT.number.isRequired,
+  image: PT.string.isRequired,
+  category: PT.string.isRequired,
 };
 
 export default GoodsItem;
--- spa-parfum/src/pages/Men.jsx
+++ spa-parfum/src/pages/Men.jsx
@@ -1,27 +1,50 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 
 import GoodsItem from '../Components/GoodsItem';
 
 const Men = () => {
-  const ul = useRef();
-  const [data, setData] = useState([]);
+  const [{ loading, success, error }, setAlerts] = useState({ loading: true });
+  const loadData = useCallback(async () => {
+    setAlerts({ loading: true });
+
+    try {
+      const data = await fetch('http://localhost:1234/goods').then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+          return response.json().then(({ error: err }) => {
+            throw Error(err || 'Oops an error occurred.');
+          });
+        }
+
+        return response.json();
+      });
+
+      setAlerts({ success: data || [] });
+    } catch (err) {
+      setAlerts({ error: err });
+    }
+  }, []);
 
   useEffect(() => {
-    fetch('http://localhost:8000/goods')
-      .then((db) => db.json())
-      .then((json) => setData(json));
-  }, [data]);
+    loadData();
+  }, [loadData]);
+
+  if (loading)
+    return (
+      <div className="goods">
+        <h3 align="center">Loading.....</h3>
+      </div>
+    );
 
   return (
     <div className="goods">
-      {data.length > 0 ? (
-        <ul ref={ul} className="goods__items">
-          {data.map((goods, index) => (
-            <GoodsItem goods={goods} />
+      {success?.length === 0 && <h3 align="center">No data</h3>}
+      {Boolean(error) && <h3 align="center">{`Error: ${error?.message ? error.message : error}`}</h3>}
+      {success?.length > 0 && (
+        <ul className="goods__items">
+          {success.map((item) => (
+            <GoodsItem key={item.id} {...item} />
           ))}
         </ul>
-      ) : (
-        <h3>Loading.....</h3>
       )}
     </div>
   );
